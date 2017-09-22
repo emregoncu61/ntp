0b584eb9158925b5358a055cf05b60d65c7b2e7c
emre <emre_goncu16@erdogan.edu.tr>
2017-09-22 20:06:18 +0300

    toplama ve bölme metodu yazıldı

---
+++ src/toplama/tolamabolme.java
@@ -0,0 +1,15 @@
+package toplama;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PC6
+ */
+public class tolamabolme {
+    
+}
---
+++ src/toplama/toplamabol.java
@@ -0,0 +1,40 @@
+/*
+    
+ */
+package toplama;
+
+/**
+ *
+ * @author PC6
+ */
+public class toplamabol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int a=5,b=6,c=0;
+//        c=a+b;
+        System.out.println(topla(5,4));
+        System.out.println(bol(100,5
+        
+        
+        ));
+         
+    }
+    public static int topla(int k,int m)
+    {
+   
+        int toplam=k+m;        
+    
+    return toplam;
+    
+    }
+    public static int bol(int s,int h)
+    {
+    int bol=s/h;
+    
+    return bol;
+    
+    }
+}
